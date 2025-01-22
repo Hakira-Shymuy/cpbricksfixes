717c101b12555153ccd33197f7a8756dc146a7c2
Hakira-Shymuy <hakira.shymuy@gmail.com>
2025-01-22 21:42:20 +0100

    chore: basic commit

---
+++ plugin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * ClassicPress Client Main file.
+ *
+ * @package ClassicPress_Client
+ */
+
+/**
+ * Plugin Name:       ClassicPressFix
+ * Plugin URI:        https://classicpress.io/
+ * Description:       A real time wireframing tool, design-ready development system, and accessible component library that empowers you to build beautiful custom websites in half the time with zero limits on your creativity.
+ * Version:           0.0.1
+ * Requires at least: 5.9
+ * Requires PHP:      7.3
+ * Author:            Kevin Geary, Matteo Greco
+ * Author URI:        https://classicpress.io/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://classicpress.io/
+ * Text Domain:       classicpressfix
+ * Domain Path:       /languages
+ */
